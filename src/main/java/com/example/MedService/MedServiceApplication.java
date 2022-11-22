adc4faef2d1301322852a7dc1076acd92796a691
pbraer <braer.a.s@gmail.com>
2022-11-22 22:39:14 +0700

    START

---
+++ src/main/java/com/example/MedService/MedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MedService/MedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
